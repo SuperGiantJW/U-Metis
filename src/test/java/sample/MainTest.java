2f6f40a5d62e48979799b7d2ce520b81a1fae7d1
Joe <Joe>
2016-11-16 13:42:56 -0600

    Initial commit

---
+++ src/test/java/sample/MainTest.java
@@ -0,0 +1,18 @@
+package sample;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Joe on 11/16/2016.
+ */
+public class MainTest {
+    @Test
+    public void start() throws Exception {
+        String user = "test";
+        boolean isAuthenticated = true;
+        assertTrue(isAuthenticated);
+    }
+
+}
